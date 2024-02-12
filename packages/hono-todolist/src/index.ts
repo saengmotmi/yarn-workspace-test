60a10ca3cec8d62870289fdebffc01ade1fdeb48
saengmotmi <ohjtack@gmail.com>
2024-02-12 23:11:48 +0900

    feature: production api 와 연결

--- packages/hono-todolist/src/index.ts
+++ packages/hono-todolist/src/index.ts
@@ -55,6 +55,22 @@ app.put("/tasks/:id", async (c) => {
   return c.json({ data });
 });
 
+app.patch("/tasks/:id", async (c) => {
+  const supabase = getSupabaseClient(c);
+
+  const body = await c.req.json();
+
+  const { data, error } = await supabase
+    .from("tasks")
+    .upsert(body)
+    .match({ id: c.req.param("id") });
+
+  if (error) {
+    return c.json({ error: error.message });
+  }
+  return c.json({ data });
+});
+
 app.post("/users/signup", async (c) => {
   const supabase = getSupabaseClient(c);
 
